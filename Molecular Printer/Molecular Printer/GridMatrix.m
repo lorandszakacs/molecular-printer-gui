4276903e1a613473556d3a211bd0917d601fe109
eyhlin <eylin@iastate.edu>
2013-11-30 03:14:11 -0600

    Implemented GridMatrix

--- Molecular Printer/Molecular Printer/GridMatrix.m
+++ Molecular Printer/Molecular Printer/GridMatrix.m
@@ -19,4 +19,46 @@
     _matrix = matrix;
     return self;
 }
+
+-(BOOL)checkXY:(NSInteger)x :(NSInteger)y{
+    if(x<0||y<0||x>=_width||y>=_height)
+        return NO;
+    return YES;
+}
+
+-(BOOL)isMarked:(NSInteger)x :(NSInteger)y{
+    if([self checkXY:x :y] == NO)
+    [NSException raise:@"Invalid value for GridMatrix::isMarked" format:@"Position (%d, %d) is invalid", x, y];
+    return _matrix[y*_width+x]==1;
+}
+
+//return YES if value of cell is changed.
+-(BOOL)mark:(NSInteger)x :(NSInteger)y{
+    if([self checkXY:x :y] == NO)
+        [NSException raise:@"Invalid value for GridMatrix::mark" format:@"Position (%d, %d) is invalid", x, y];
+    if(_matrix[y*_width+x]==1)
+        return NO;
+    _matrix[y*_width+x] = 1;
+    return YES;
+}
+
+-(BOOL)unmark:(NSInteger)x :(NSInteger)y{
+    if([self checkXY:x :y] == NO)
+        [NSException raise:@"Invalid value for GridMatrix::mark" format:@"Position (%d, %d) is invalid", x, y];
+    if(_matrix[y*_width+x]==0)
+        return NO;
+    _matrix[y*_width+x] = 0;
+    return YES;
+}
+
+-(BOOL)flip:(NSInteger)x :(NSInteger)y{
+    if([self checkXY:x :y] == NO)
+        [NSException raise:@"Invalid value for GridMatrix::mark" format:@"Position (%d, %d) is invalid", x, y];
+    if(_matrix[y*_width+x]==0){
+        _matrix[y*_width+x] = 1;
+    }else
+        _matrix[y*_width+x] = 0;
+    return YES;
+}
+
 @end
